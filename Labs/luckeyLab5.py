00d086a55801331bb73914e9c9c9cf48d384fad5
vluckey1986 <torismith935@yahoo.com>
2026-05-14 19:04:50 -0400

    Update luckeyLab5.py

--- Labs/luckeyLab5.py
+++ Labs/luckeyLab5.py
@@ -1,12 +1,12 @@
 ''' 
 -----------------------------------------------------------------------------
-Solution:       luckeyLab5.py
+Solution:       
 -----------------------------------------------------------------------------
-Developer:      Victoria Luckey
-Course:         Intro to Programming & Logic - CITC-1301
-Creation Date:  10/01/2025
-Last Mod Date:  10/07/2025
-E-mail Address: vluckey@senators.ws.edu
+Developer:      
+Course:         
+Creation Date:  
+Last Mod Date: 
+E-mail Address: 
 -----------------------------------------------------------------------------
 Purpose - Get human age from user
 -----------------------------------------------------------------------------
